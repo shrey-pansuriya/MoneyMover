51e9b8c874fa90d77cc5c70a627d824b41be06e8
shrey-pansuriya <shreypansuriya11@gmail.com>
2024-11-12 05:20:49 -0500

    sp13

--- app/screens/DemoCommunityScreen.tsx
+++ app/screens/DemoCommunityScreen.tsx
@@ -28,7 +28,7 @@ export const DemoCommunityScreen: FC<DemoTabScreenProps<"DemoCommunity">> =
           <View
             style={[
               $subscriptionOption,
-              { backgroundColor: "#2f2f2f", opacity: selectedPlan === "basic" ? 0.5 : 1 }
+              { backgroundColor: "#2f2f2f", opacity: selectedPlan === "basic" ? 1 : 1 }
             ]}
           >
             <Text style={[$planName, { color: "#fff" }]}>BASIC - $200</Text>
@@ -67,7 +67,7 @@ export const DemoCommunityScreen: FC<DemoTabScreenProps<"DemoCommunity">> =
           <View
             style={[
               $subscriptionOption,
-              { backgroundColor: "#2ecc71", opacity: selectedPlan === "premium" ? 0.5 : 1 }
+              { backgroundColor: "#2ecc71", opacity: selectedPlan === "premium" ? 1 : 1 }
             ]}
           >
             <Text style={[$planName, { color: "#000" }]}>PREMIUM - $300</Text>
@@ -106,7 +106,7 @@ export const DemoCommunityScreen: FC<DemoTabScreenProps<"DemoCommunity">> =
           <View
             style={[
               $subscriptionOption,
-              { backgroundColor: "#f39c12", opacity: selectedPlan === "gold" ? 0.5 : 1 }
+              { backgroundColor: "#f39c12", opacity: selectedPlan === "gold" ? 1 : 1 }
             ]}
           >
             <Text style={[$planName, { color: "#000" }]}>GOLD - $400</Text>
